3e1b69ad55c6bb5a3b8f82d00e9f4c757c43f867
tkelly <tkelly@scottlogic.co.uk>
2015-04-22 17:17:14 +0100

    Implement tweet measurer
    Adds the methods for calculating remaining character count and corresponding colour

--- Week 2/calculator/src/main/scala/calculator/TweetLength.scala
+++ Week 2/calculator/src/main/scala/calculator/TweetLength.scala
@@ -3,13 +3,19 @@ package calculator
 object TweetLength {
   final val MaxTweetLength = 140
 
-  def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] = {
-    ???
-  }
+  def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] =
+  	Var(MaxTweetLength - tweetLength(tweetText.apply()))
 
-  def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] = {
-    ???
-  }
+  def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] =
+    Var(
+	    remainingCharsCount.apply() match {
+	      case x if x >= 15 => "green"
+	      case x if x >= 0 => "orange"
+	      case _ => "red"
+	    }
+    )
+      
+      
 
   /** Computes the length of a tweet, given its text string.
    *  This is not equivalent to text.length, as tweet lengths count the number
