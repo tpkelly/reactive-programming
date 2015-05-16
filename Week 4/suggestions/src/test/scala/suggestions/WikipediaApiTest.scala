44435b9c49aa81650d1505f64e0b0ecc25ae5d97
Tom Kelly <tkelly@scottlogic.co.uk>
2015-05-16 16:53:43 +0100

    Upload and complete Week 4
    Oops, forgot to actually save this in the first place... ah well!

---
+++ Week 4/suggestions/src/test/scala/suggestions/WikipediaApiTest.scala
@@ -0,0 +1,86 @@
+package suggestions
+
+
+
+import language.postfixOps
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Try, Success, Failure}
+import rx.lang.scala._
+import org.scalatest._
+import gui._
+import scala.collection.mutable.Buffer
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class WikipediaApiTest extends FunSuite {
+
+  object mockApi extends WikipediaApi {
+    def wikipediaSuggestion(term: String) = Future {
+      if (term.head.isLetter) {
+        for (suffix <- List(" (Computer Scientist)", " (Footballer)")) yield term + suffix
+      } else {
+        List(term)
+      }
+    }
+    def wikipediaPage(term: String) = Future {
+      "Title: " + term
+    }
+  }
+
+  import mockApi._
+
+  test("WikipediaApi should make the stream valid using sanitized") {
+    val notvalid = Observable.just("erik", "erik meijer", "martin")
+    val valid = notvalid.sanitized
+
+    var count = 0
+    var completed = false
+
+    val sub = valid.subscribe(
+      term => {
+        assert(term.forall(_ != ' '))
+        count += 1
+      },
+      t => assert(false, s"stream error $t"),
+      () => completed = true
+    )
+    assert(completed && count == 3, "completed: " + completed + ", event count: " + count)
+  }
+  
+  test("recovered") {
+    val thrown = new Throwable
+    val requests = Observable.just(1, 2, 3) ++ Observable.error(thrown)
+    val responses = requests recovered
+    
+    val observed = Buffer[Any]()
+
+    val sub = responses subscribe {
+      observed += _
+    }
+
+    assert(observed == Seq(Success(1), Success(2), Success(3), Failure(thrown)), observed)
+  }
+  
+  test("WikipediaApi should correctly use concatRecovered") {
+    val requests = Observable.just(1, 2, 3)
+    val remoteComputation = (n: Int) => Observable.just(0 to n : _*)
+    val responses = requests concatRecovered remoteComputation
+    val sum = responses.foldLeft(0) { (acc, tn) =>
+      tn match {
+        case Success(n) => acc + n
+        case Failure(t) => throw t
+      }
+    }
+    var total = -1
+    val sub = sum.subscribe {
+      s => total = s
+    }
+    assert(total == (1 + 1 + 2 + 1 + 2 + 3), s"Sum: $total")
+  }
+
+}
